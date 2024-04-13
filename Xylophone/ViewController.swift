4d5fb5007c173707f4a5c78076456ecfa307b969
Stavros <snikoletopoulos@hotmail.com>
2024-04-13 15:02:54 +0300

    Play note on each button press

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,13 +1,24 @@
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.layer.opacity = 0.5
+
+        if let note = sender.currentTitle {
+            playSound(note: note)
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            sender.layer.opacity = 1
+        }
     }
 
-
-    
-
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
-
