bea96a7d8f270588d05b7d25534f09af47499f2d
Stavros <snikoletopoulos@hotmail.com>
2024-04-15 01:58:41 +0300

    Format code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -2,23 +2,23 @@ import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-    var player: AVAudioPlayer!
+  var player: AVAudioPlayer!
 
-    @IBAction func keyPressed(_ sender: UIButton) {
-        sender.layer.opacity = 0.5
+  @IBAction func keyPressed(_ sender: UIButton) {
+    sender.layer.opacity = 0.5
 
-        if let note = sender.currentTitle {
-            playSound(note: note)
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            sender.layer.opacity = 1
-        }
+    if let note = sender.currentTitle {
+      playSound(note: note)
     }
 
-    func playSound(note: String) {
-        let url = Bundle.main.url(forResource: note, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+      sender.layer.opacity = 1
     }
+  }
+
+  func playSound(note: String) {
+    let url = Bundle.main.url(forResource: note, withExtension: "wav")
+    player = try! AVAudioPlayer(contentsOf: url!)
+    player.play()
+  }
 }
